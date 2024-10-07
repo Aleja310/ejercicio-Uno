c9879f16baabdd7985cdb39be640585a66cac193
aleja-tovar <camilatovarcamilatovar6@hotmail.com>
2024-10-06 22:37:57 -0500

    estructuras secuenciales

---
+++ javascript/script.js
@@ -0,0 +1,75 @@
+//Estructuras secuenciales 
+
+//TAREA1
+
+/*let hombres = 0;
+let mujeres = 0;
+let totalEstudiantes = 0;
+let porcentaje = 0;
+let porcentajeM = 0;
+let porcentajeF = 0;
+
+hombres = Number(prompt('Ingrese el número de estudiantes masculinos'));
+mujeres = Number(prompt('Ingrese el número de estudiantes femeninos'));
+
+totalEstudiantes = hombres + mujeres;
+
+porcentaje = hombres / totalEstudiantes;
+porcentaje = mujeres / totalEstudiantes;
+porcentajeM = porcentaje * 100;
+porcentajeF = porcentaje * 100;
+
+alert('el porcentaje de hombres es:' + porcentajeM.toFixed(2));
+alert('el porcentaje de mujeres es:' + porcentajeF.toFixed(2));*/
+
+
+
+//TAREA2
+
+
+ /*let metro= 0;
+ let pulgadas= 0.0254;
+ let resultado=0;
+
+ metro=Number(prompt('Ingrese los metros a convertir:'));
+
+ resultado= metro / pulgadas;
+
+ alert( resultado.toFixed(3) + 'pulgadas.');*/
+
+
+ //TAREA3
+
+
+ /*function CalcularcalificacionFinal(){
+
+    let calificaciones = "c1,c2,c3";
+    let examenFinal=0;
+    let trabajoFinal=0;
+    let calificacionFinal=0;
+
+    c1 = Number (prompt ('Ingrese la calificación parcial 1: '));
+    c2 = Number (prompt ('Ingrese la calificación parcial 2: '));
+    c3 = Number (prompt ('Ingrese la calificación parcial 3: '));
+
+examenFinal=Number (prompt('Ingrese la calificación del examen final: '));
+trabajoFinal=Number (prompt('Ingrese la calificación del trabajo final: '));
+
+ calificaciones= (c1 + c2 + c3 ) / 3;
+  calificacionFinal= (calificaciones * 0.55) + (examenFinal * 0.30) + (trabajoFinal * 0.15);
+
+  alert(`La calificación final es: ${calificacionFinal.toFixed(2)}`);
+}
+CalcularcalificacionFinal();*/
+
+
+
+ 
+
+  
+
+
+
+
+
+ 
